1ac891f5ae77b3c92fdbb52b0bd5248a9b6191e7
GabrielRCodes <gabrielrcodes@gmail.com>
2023-05-10 21:46:11 -0300

    Finalizando route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -52,5 +52,14 @@ export const authOptions: AuthOptions = {
         return user
       }
     })
-  ]
-}
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST };
