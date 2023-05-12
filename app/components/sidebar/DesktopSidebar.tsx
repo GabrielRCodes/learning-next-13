99a43d85d5e47e37f008163918436f7fed2dd58e
GabrielRCodes <gabrielrcodes@gmail.com>
2023-05-12 13:35:54 -0300

    Finishing the first part of sidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,6 +3,7 @@
 import useRoutes from '@/app/hooks/useRoutes'
 import { User } from '@prisma/client';
 import React, { useState } from 'react'
+import Avatar from '../Avatar';
 import DesktopItem from './DesktopItem'
 
 interface DesktopSidebarProps {
@@ -65,6 +66,26 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
             />
           ))}
         </ul>
+      </nav>
+      <nav
+        className="
+          mt-4
+          flex
+          flex-col
+          justify-between
+          items-center
+        "
+      >
+        <div
+          onClick={() => setIsOpen(true)}
+          className="
+            cursor-pointer
+            hover:opacity-75
+            transition
+          "
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   )
